e28e564cb1f34927b0e134c14d9736a22f66660f
Noa-f <ferault.noa@gmail.com>
2024-10-11 23:32:39 +0200

    adding files to git

---
+++ Scrypt/script.js
@@ -0,0 +1,39 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("environnementBtn").addEventListener("click", function() {
+        window.location.href = "Environnement.html";
+    });
+
+    document.getElementById("emploieBtn").addEventListener("click", function() {
+        window.location.href = "Emploie.html";
+    });
+
+    document.getElementById("individuBtn").addEventListener("click", function() {
+        window.location.href = "Individu.html";
+    });
+        document.getElementById("economieBtn").addEventListener("click", function() {
+        window.location.href = "Economie.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("retourbtn").addEventListener("click", function() {
+        window.location.href = "Information.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("accueilBtn").addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+    document.getElementById("tierslBtn").addEventListener("click", function() {
+        window.location.href = "Tiers.html";
+    });
+    document.getElementById("formulaireBtn").addEventListener("click", function() {
+        window.location.href = "Formulaire.html";
+    });
+    document.getElementById("informationBtn").addEventListener("click", function() {
+        window.location.href = "Information.html";
+    });
+    document.getElementById("statistiqueBtn").addEventListener("click", function() {
+        window.location.href = "Statistique.html";
+    });
+});
